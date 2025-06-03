29aced04313a58d53a9c64a96c2c55eadbd0a115
8514a <ann8514a@gmail.com>
2025-06-03 14:14:54 +0300

    am finisat etapa autentificarii cu ajutorul clerk, am adaugat userbutton care ne arata contul prin care ne-am logat si de asemenea ne permite sa accesam detaliile contului

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,11 @@
-import { Button } from "@/components/ui/button";
-export default function Home() {
+import { UserButton } from "@clerk/nextjs";
+const SetupPage = () => {
   return (
-   <div className="p-4" >
-     <Button size="default" variant="destructive">Click me</Button>
+   <div className="p-4">
+     <UserButton afterSignOutUrl="/" />
     </div>
     
   );
 }
+
+export default SetupPage;
